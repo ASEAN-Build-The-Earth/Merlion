96acfebe23af63c4ce29ece18b7e6a6d4484c7bd
StoneMcYT <67421882+StoneMcYT@users.noreply.github.com>
2021-08-17 09:25:00 +0800

    Update 1rule.js
    Work in pogress

--- commands/1rule.js
+++ commands/1rule.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js');
 module.exports = {
-  name: 'rulesg ',
-  description: 'The rule of SG BTE',
+  name: 'mrules ',
+  description: 'The rule of the main BTE server',
   execute(message){
 const Rules = new Discord.MessageEmbed()
 .setColor('#00FFFF')
 .setAuthor(`${message.author.username}`, `${message.author.displayAvatarURL({ dynamic: true })}`)
 .setTitle (' Singapore Build The Earth Rules')
     .addFields( 
-      { name: "Rule #1 Respect:", value: "Please treat everyone with respect. \nToxicity, racism, impersonation, prejudice or any other negative points are disallowed in this community. " },
-      { name: "Rule #2 Discussion", value: "Discussions of heavy topics such as politics are not allowed in this community. We highly encourage discussions related to Singapore BTE." },
-      { name: "Rule #3 Content or Language", value: "NSFW, explicit and provocative content or language are strictly forbidden in this community. Examples include pictures, links, videos, emojis, reactions and usernames. This also includes suggestive content or language." },
+      { name: ":globe_with_meridians: 1. All forms of racism, sexism, homophobia, and other forms of prejudice are prohibited.", value: "Includes voice, text channels, direct messages, with no exceptions." },
+      { name: ":underage: 2. Sending any attachment (video/image/link) that contains erotic/sexual content or gore is forbidden.", value: "— Suggestive/Minor NSFW in text, voice, emojis and reactions is also not allowed, but is taken more lightly." },
+      { name: ":bank: 3. Talk of politics and other heavy topics is prohibited in all channels. ", value: "This is a server dedicated to the Build The Earth project alone, and we do not allow heavy or dividing topics to be discussed." },
       { name: "Rule #4 Discord's Terms of Service", value: "Follow the Discord's Terms of Service to keep you out of trouble.\n Do not ACT like you're underage, it will seem suspicious." },
       { name: "Rule #5 Channels", value: "Use the channels for it's intended purposes. Do not, for example, use #progress-images for your usual chatting." },
       { name: "Rule #6 Finding Help", value: "Please DO NOT spam or cause any disruption, including mass pinging. If you require assistance, please create a support ticket with <@724495837325164576>" },
