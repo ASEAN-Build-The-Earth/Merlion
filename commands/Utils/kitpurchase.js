415e96d8be786d6ed8fd6ce965eb902905f9b9cc
S4ge-Rites <54899587+S4ge-Rites@users.noreply.github.com>
2021-10-14 23:11:40 +0800

    done

--- commands/Utils/kitpurchase.js
+++ commands/Utils/kitpurchase.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'kitpurchase',
     description: 'link for covid19 test kit',
-    aliases: ['Buytestkit'],
+    aliases: ['Buykit'],
     execute(message, args) {
         message.reply('https://www.amazon.com/Simplicity-COVID-19-PCR-Home-Testing/dp/B08XMVL6V1/ref=sr_1_2?dchild=1&keywords=covid%2Btest%2Bkit&qid=1634224122&sr=8-2&th=1');
     },
