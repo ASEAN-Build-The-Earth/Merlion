3051aa1adea61a7e162120daaa857360cb4eceb1
S4ge-Rites <54899587+S4ge-Rites@users.noreply.github.com>
2021-10-14 22:34:24 +0800

    commited for kit.js command

---
+++ commands/System/kit.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'kit',
+    description: 'Kit faq',
+    alias: ['kit'],
+    execute(message, args) {
+        message.reply('https://www.moh.gov.sg/covid-19/selftestart');
+    },
+};
