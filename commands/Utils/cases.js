98997b6b412eb084e18abd82fe2ddb078e54a76f
S4ge-Rites <54899587+S4ge-Rites@users.noreply.github.com>
2021-10-15 23:16:14 +0800

    ok

--- commands/Utils/cases.js
+++ commands/Utils/cases.js
@@ -1,7 +1,10 @@
 module.exports = {
     name: "cases",
     execute(message, args) {
+        if (args.length > 2) return message.reply("Too many arguments...")
+        if (args.length <= 1) return message.reply("Not enough arguments")
         
+
         axios.get(`https://api.covid19api.com/total/country/${String(args[1])}`)
             .then(response => 
             {
