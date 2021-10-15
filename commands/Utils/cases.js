a52d443217ed7b56241a1e4616d251417794f9e7
S4ge-Rites <54899587+S4ge-Rites@users.noreply.github.com>
2021-10-15 22:41:32 +0800

    ok

--- commands/Utils/cases.js
+++ commands/Utils/cases.js
@@ -1,12 +1,27 @@
-const axios = require('axios')
-
 module.exports = {
     name: "cases",
     execute(message, args) {
-        if (args.length > 2) return message.reply("Too many arguments...")
-        if (args.length <= 1) return message.reply("Not enough arguments")
-        axios.get(`https://api.covid19api.com/total/country/${args[1]}`)
-            .then(response => {
-                console.log(response.data);
-            })
-    }}
+        
+        axios.get(`https://api.covid19api.com/total/country/${String(args[1])}`)
+            .then(response => 
+            {
+                message.reply(`Confirmed: ${response.data.Confirmed},
+                Deaths: ${response.data.Deaths}, 
+                Recovered: ${response.data.Recovered}, 
+                Active: ${response.data.Active}`)
+            }
+        ).catch(error => 
+            {
+                console.error(`Could not send Cases DM to ${message.author.tag}.\n`, error);
+                message.reply(`Error found: https://api.covid19api.com/total/country/${args[1]} \n` +  `\`\`\`\n` 
+                + error + "\n" 
+                + error.response.data  + "\n" 
+                + error.response.status + "\n" 
+                + error.response.headers + "\n" 
+                + `\`\`\`\n`);
+                // message.reply('it seems like I can\'t DM you!');
+            }
+            
+        );
+    }
+}
