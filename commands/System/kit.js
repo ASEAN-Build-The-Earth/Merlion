dfbc8aaac2fa133860b48fa1def03fe3a6e13547
S4ge-Rites <54899587+S4ge-Rites@users.noreply.github.com>
2021-10-16 16:05:57 +0800

    Deleted covid 19 related self test kit files

--- commands/System/kit.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'kit',
-    description: 'Kit faq',
-    alias: ['kit'],
-    execute(message, args) {
-        message.reply(' The following link is only for singaporeans  https://www.moh.gov.sg/covid-19/selftestart');
-    },
-};
--- commands/Utils/kitpurchase.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'kitpurchase',
-    description: 'link for covid19 test kit',
-    aliases: ['buykit'],
-    execute(message, args) {
-        message.reply('https://www.amazon.com/Simplicity-COVID-19-PCR-Home-Testing/dp/B08XMVL6V1/ref=sr_1_2?dchild=1&keywords=covid%2Btest%2Bkit&qid=1634224122&sr=8-2&th=1');
-    },
-};
