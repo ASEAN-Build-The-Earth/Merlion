4c7ecdc45c3c0844fe11a47e6c991c4161095756
S4ge-Rites <54899587+S4ge-Rites@users.noreply.github.com>
2021-10-17 22:54:32 +0800

    done

--- index.js
+++ index.js
@@ -35,7 +35,21 @@ for (const folder of commandFolders) {
         client.commands.set(command.name, command);
     }
 }
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
 
+	if (interaction.commandName === 'ping') {
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('primary')
+					.setLabel('Primary')
+					.setStyle('PRIMARY'),
+			);
+
+		await interaction.reply({ content: 'Pong!', components: [row] });
+	}
+});
 
 client.once('ready', () => {
     console.log('Ready!');
@@ -89,21 +103,7 @@ client.on('messageCreate', message => {
     const Discord = require('discord.js')
 const { MessageActionRow, MessageButton, } = require('discord.js');
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
 
-	if (interaction.commandName === 'ping') {
-		const row = new MessageActionRow()
-			.addComponents(
-				new MessageButton()
-					.setCustomId('primary')
-					.setLabel('Primary')
-					.setStyle('PRIMARY'),
-			);
-
-		await interaction.reply({ content: 'Pong!', components: [row] });
-	}
-});
 });
 
 client.login(token)
