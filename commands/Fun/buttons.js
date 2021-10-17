a5818b2d1e7a9cde1a4f5499f2e89e22694e070b
S4ge-Rites <54899587+S4ge-Rites@users.noreply.github.com>
2021-10-17 21:56:21 +0800

    Added buttons.js (test)

--- commands/Fun/buttons.js
+++ commands/Fun/buttons.js
@@ -1,5 +1,25 @@
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js')
+const { execute } = require('./8ball')
 module.exports = {
     name: 'test1 for buttons',
-    description: 'test for buttons feature'
-    
+    description: 'test for buttons feature',
+    execute: async(client, message, args) => {
+        const row = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+                .setCustomId('primary')
+                .setLabel("Primary")
+                .setStyle("PRIMARY")
+                );
+
+            let embed = new MessageEmbed()
+            .setTitle("Buttons Example")
+            .setDescription("This is Button Description")
+    message.channel.send({
+        embeds: [embed],
+        components: [row]
+
+    })
+            }
+
 }
