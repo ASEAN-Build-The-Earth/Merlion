b6eba827d94017ae9333861a4d535020f33f816c
Tintinkung <77855014+tintinkung@users.noreply.github.com>
2021-10-20 00:32:42 +0700

    edit: cases.js command
    + error catching code
    + added some embed feature
    + some docs for the command
    - simplified embed error catch code

--- commands/Utils/cases.js
+++ commands/Utils/cases.js
@@ -1,38 +1,46 @@
 const Discord = require('discord.js')
 const axios = require('axios');
+
 module.exports = {
     name: "cases",
-    execute(message, args) {
-        
-        axios.get(`https://disease.sh/v3/covid-19/countries/${String(args[0])}`)
-            .then(response => 
+    execute(message, args) 
+    {
+        // --- Advaliable countries: https://github.com/disease-sh/API/blob/master/utils/countries.js
+
+        // generated link for grabbing data
+        let generatedLink = `https://disease.sh/v3/covid-19/countries/${String(args[0])}`;
+        // shorterned link for printing text only(reduce word size)
+        let shortLink = `disease.sh/v3/covid-19/countries/${String(args[0])}`
+
+    // Error if we cant detect command's arguments
+    if(args[0] === undefined) { message.reply(`this command needed an arguments`); return; }
+
+        // --- Start Generating Embed Data ---
+        axios.get(generatedLink)
+        .then(response => 
             {
                 const embed = new Discord.MessageEmbed()
                 .setColor("#A8542D")
-                .setTitle("COVID-19 statistics")
+                .setTitle("COVID-19 statistics").setURL("https://disease.sh")
+                .setDescription(`*data: [${shortLink}](${generatedLink})*`)
                 .setThumbnail(`${response.data.countryInfo.flag}`)
                 	.addFields(
-                    { name: 'Total cases', value: `${response.data.cases}`},
-                    { name: 'Total cases reported today', value: `${response.data.todayCases}`, inline: true},
-		                { name: 'Total deaths', value: `${response.data.deaths}`},
-		                { name: 'Total deaths reported today', value: `${response.data.todayDeaths}`, inline: true },
-                    { name: 'Total recoveries', value: `${response.data.recovered}`},
-                    { name: 'Total recoveries reported today', value: `${response.data.todayRecovered}`, inline: true},
-                    { name: 'Active cases', value: `${response.data.active}`},
-                    {name: 'Critical cases', value: `${response.data.critical}`, inline: true},)
-                message.reply({ embeds: [embed]});}
-        ).catch(error => 
-            {
-                console.error(`Could not send Cases DM to ${message.author.tag}.\n`, error);
-                message.reply(`Error found: https://api.covid19api.com/total/country/${String(args[0])} \n` +  `\`\`\`\n` 
-                + error + "\n" 
-                + error.response.data  + "\n" 
-                + error.response.status + "\n" 
-                + error.response.headers + "\n" 
-                + `\`\`\`\n`);
-                // message.reply('it seems like I can\'t DM you!');
+                    { name: 'Total cases:', value: `${response.data.cases}`},
+                    { name: 'Total cases reported today:', value: `${response.data.todayCases}`, inline: true},
+		            { name: 'Total deaths:', value: `${response.data.deaths}`},
+		            { name: 'Total deaths reported today:', value: `${response.data.todayDeaths}`, inline: true },
+                    { name: 'Total recoveries:', value: `${response.data.recovered}`},
+                    { name: 'Total recoveries reported today:', value: `${response.data.todayRecovered}`, inline: true},
+                    { name: 'Active cases:', value: `${response.data.active}`},
+                    {name: 'Critical cases:', value: `${response.data.critical}`, inline: true},)
+                .setFooter("disease.sh - Open Disease Data API");
+                message.reply({ embeds: [embed]});
             }
-            
+        ).catch(error => //catch error if embed generating failed
+            {
+                console.error(`Could not send Cases Message to ${message.author.tag}.\n`, error);
+                message.reply(`Invalid Input: ${generatedLink} \n`);
+            }
         );
     }
 }
