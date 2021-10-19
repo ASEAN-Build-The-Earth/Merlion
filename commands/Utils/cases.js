68fc5b70aa0a6f4da6a0e6851e2c39173eb28227
Kira <90365542+Kira272921@users.noreply.github.com>
2021-10-19 19:34:08 +0530

    Updated the API as well added an embed

--- commands/Utils/cases.js
+++ commands/Utils/cases.js
@@ -1,23 +1,30 @@
+const Discord = require('discord.js')
 const axios = require('axios');
 module.exports = {
     name: "cases",
     execute(message, args) {
         
-        axios.get(`https://api.covid19api.com/total/country/${String(args[0])}`)
+        axios.get(`https://disease.sh/v3/covid-19/countries/${String(args[0])}`)
             .then(response => 
             {
-
-                message.reply(`Data: https://api.covid19api.com/total/country/${String(args[0])}
-                Confirmed: ${response.data[response.data.length - 1].Confirmed},
-                Deaths: ${response.data[response.data.length - 1].Deaths}, 
-                Recovered: ${response.data[response.data.length - 1].Recovered}, 
-                Active: ${response.data[response.data.length - 1].Active}`)
-                
-            }
+                const embed = new Discord.MessageEmbed()
+                .setColor("#A8542D")
+                .setTitle("COVID-19 statistics")
+                .setThumbnail(`${response.data.countryInfo.flag}`)
+                	.addFields(
+                    { name: 'Total cases', value: `${response.data.cases}`},
+                    { name: 'Total cases reported today', value: `${response.data.todayCases}`, inline: true},
+		                { name: 'Total deaths', value: `${response.data.deaths}`},
+		                { name: 'Total deaths reported today', value: `${response.data.todayDeaths}`, inline: true },
+                    { name: 'Total recoveries', value: `${response.data.recovered}`},
+                    { name: 'Total recoveries reported today', value: `${response.data.todayRecovered}`, inline: true},
+                    { name: 'Active cases', value: `${response.data.active}`},
+                    {name: 'Critical cases', value: `${response.data.critical}`, inline: true},)
+                message.reply({ embeds: [embed]});}
         ).catch(error => 
             {
                 console.error(`Could not send Cases DM to ${message.author.tag}.\n`, error);
-                message.reply(`Error found: https://api.covid19api.com/total/country/${String(args[0])} \n` +  `\`\`\`\n` 
+                message.reply(`Error found: https://disease.sh/v3/covid-19/countries/${String(args[0])} \n` +  `\`\`\`\n` 
                 + error + "\n" 
                 + error.response.data  + "\n" 
                 + error.response.status + "\n" 
