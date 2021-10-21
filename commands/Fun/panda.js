bd10043ea37ef4d288a8927a6c507a00ea4dc877
Kira <90365542+Kira272921@users.noreply.github.com>
2021-10-21 16:19:43 +0530

    Panda image command

---
+++ commands/Fun/panda.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const Discord = require('discord.js')
+
+module.exports = {
+    name: 'panda',
+    description: 'Uses an API to grab panda images ',
+    execute(message, args) {
+        axios.get('https://some-random-api.ml/img/panda')
+            .then(async response => {
+                const Msg = new Discord.MessageEmbed()
+                    .setDescription("Here is your panda, uwu")
+                    .setColor("#e6f5ff")
+                    .setImage(response.data.link)
+                message.reply({ embeds: [Msg] });
+            });
+    },
+};
