0cf329f0d1f97e2e1cbc04818c10684df22c5d3d
Tintinkung <sukrit_dinosuar@hotmail.com>
2022-03-31 02:59:11 +0700

    [Feat] symplified embed fetching command
    *symplified embed command as for [bcb2fcff18da73a34cc24aebf9c1620bfcc20810]
    
    <tintinkung.lemonade@gmail.com>

--- commands/fun/joke.js
+++ commands/fun/joke.js
@@ -1,7 +1,8 @@
 const { Command } = require("@sapphire/framework");
-const { send, get } = require("@sapphire/plugin-editable-commands");
 const axios = require('axios');
-const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+const { fetch, FetchResultTypes } = require('@sapphire/fetch');
+const { SendEmbed } = require("#util/embed.js");
 
 class NewCommand extends Command {
 	constructor(context, options) {
@@ -12,38 +13,20 @@ class NewCommand extends Command {
 		});
 	}
 
-	async messageRun(message) 
-	{
-        let success = false;
-        const { logger } = this.container;
-        const temp = new Discord.MessageEmbed()
-            .setColor("#b3b3ff")
-            .setDescription("*thinking joke*");
-        // send await embed waiting for bot to grab api
-        await send(message, { embeds: [temp] });
+	async messageRun(message) {
+        const pendingEmbed = new SendEmbed(message)
+        await pendingEmbed.sendPendingEmbed("*thinking joke*")
 
-        setTimeout(() => {
-            const errorEmbed = new Discord.MessageEmbed()
-                .setColor("#ff1a1a") // red
-                .setDescription("*sorry, I cant think of any joke*");
-            // send error if takes too long to respons
-            if(success) return;
-            return get(message).edit({ embeds: [errorEmbed] });
-        }, 10000/*10 secs*/);
-
-        
-        axios.get("https://some-random-api.ml/joke")
-        .then(async response => {
-            const joke = new Discord.MessageEmbed()
-            .setColor("#b3b3ff")
-            .setDescription(response.data.joke)
-
-            // edit the embed with grabbed joke
-            return get(message).edit({ embeds: [joke] }).then(() => { success = true; });
-        }).catch((error) => {
-            logger.error(`Could not send Cases Message to ${message.author.tag}.\n`, error);
+        fetch("https://some-random-api.ml/joke", FetchResultTypes.JSON)
+        .then(async (response) => {
+            const jokeEmbed = new MessageEmbed().setColor("#b3b3ff").setDescription(response.joke)
+            pendingEmbed.resolve(jokeEmbed);
+        })
+        .catch((error) => {
+            const errorEmbed = new MessageEmbed().setColor("#ff1a1a").setDescription("*sorry, I cant think of any joke*");
+            pendingEmbed.reject({errorEmbed: errorEmbed, error: { data: error, message:"failed to fetch joke api" }})
         });
-	}//end of command
+	}
 }
 
 module.exports.NewCommand = NewCommand;
